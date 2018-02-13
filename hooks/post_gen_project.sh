8f17197c9892a2f5526dca65f9a1a9508241e59d
Elijah C <ejd.christensen@gmail.com>
2018-02-12 20:09:01 -0700

    fixed shebang

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,3 +1,5 @@
+# /usr/bin/bash
+
 # virtual environment management with pyenv
 pyenv virtualenv {{cookiecutter.python_version}} {{cookiecutter.project_name}}
 pyenv local {{cookiecutter.project_name}}
