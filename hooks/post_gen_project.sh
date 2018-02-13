d4b556fc0d424eaf4d9ce2ec13834fb31ddfeb6d
Elijah C <ejd.christensen@gmail.com>
2018-02-12 20:11:33 -0700

    broken hooks, will fix later

--- hooks/post_gen_project.sh
+++
@@ -1,7 +0,0 @@
-# /usr/bin/bash
-
-# virtual environment management with pyenv
-pyenv virtualenv {{cookiecutter.python_version}} {{cookiecutter.project_name}}
-pyenv local {{cookiecutter.project_name}}
-
-pip install -r requirements.txt
