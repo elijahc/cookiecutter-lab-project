7f883920d1c76aba7fc7ad43461f2d5787a4e067
Elijah C <ejd.christensen@gmail.com>
2018-02-12 20:06:38 -0700

    updated pyenv hooks

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,5 @@
 # virtual environment management with pyenv
-pyenv virtualenv {{cookiecutter.py_version}} {{cookiecutter.project_name}}
+pyenv virtualenv {{cookiecutter.python_version}} {{cookiecutter.project_name}}
 pyenv local {{cookiecutter.project_name}}
 
 pip install -r requirements.txt
